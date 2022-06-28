e9885775590acdc14b9b20f1fd00b8027954dfb4
mariamhassann <104035552+mariamhassann@users.noreply.github.com>
2022-06-28 14:24:27 +0200

    GSEA stand 28.06

--- project/GSEA.R
+++ project/GSEA.R
@@ -1,8 +1,8 @@
 ## GSEA trial
 # install packages
-BiocManager::install("clusterProfiler", update = FALSE)
-BiocManager::install("msigdbr", update = FALSE)
-BiocManager::install("org.Mm.eg.db", update = FALSE)
+#BiocManager::install("clusterProfiler", update = FALSE)
+#iocManager::install("msigdbr", update = FALSE)
+#BiocManager::install("org.Mm.eg.db", update = FALSE)
 
 ## library
 library(clusterProfiler)
@@ -16,7 +16,7 @@ library(org.Mm.eg.db)
 # We will need this so we can use the pipe: %>%
 library(magrittr)
 
-load("fit_matched2.tsv")
+load("fit_matched2.RData")
 
 #projectPath <- dirname(rstudioapi::getSourceEditorContext()$path)
 file.exists("fit.tsv")
@@ -26,14 +26,15 @@ Mm_hallmark_df <- msigdbr(
   category = "H")
 head(Mm_hallmark_df)
 keytypes(org.Mm.eg.db)
-dge_heart= subset(dge_df, ...1 %in% heart$`Gene Script`)
+dge_heart= subset(dge_df, rownames(dge_df) %in% heart$`Gene Script`)
+
 # First let's create a mapped data frame we can join to the differential
 # expression stats
 dge_mapped_df <- data.frame(
   gene_symbols = mapIds(
     # Replace with annotation package for the organism relevant to your data
     org.Mm.eg.db,
-    keys = dge_df$...1,
+    keys =rownames(dge_heart),
     # Replace with the type of gene identifiers in your data
     keytype = "ENSEMBLTRANS",
     # Replace with the type of gene identifiers you would like to map to
@@ -48,7 +49,7 @@ dge_mapped_df <- data.frame(
   # Make an `Ensembl` column to store the rownames
   tibble::rownames_to_column("Ensembltrans") %>%
   # Now let's join the rest of the expression data
-  dplyr::inner_join(dge_heart, by = c("Ensembltrans" = "...1"))
+  dplyr::inner_join(dge_heart, by = c("Ensembltrans" = rownames(dge_heart)))
 head(dge_mapped_df)
 #The goal of GSEA is to detect situations where many genes in a gene set change in a coordinated way, even when individual changes are small in magnitude (Subramanian et al. 2005).
 
@@ -59,34 +60,36 @@ dup_gene_symbol <- dge_mapped_df %>%
 
 view(dup_gene_symbol)
 #t test
-t.test(dge_mapped_df)
+
 filtered_dge_mapped_df <- dge_mapped_df %>%
   # Sort so that the highest absolute values of the t-statistic are at the top
-  dplyr::arrange(dplyr::desc(abs(F))) %>%
+  dplyr::arrange(dplyr::desc(abs(dge_mapped_df[,11]))) %>%
   # Filter out the duplicated rows using `dplyr::distinct()`-- this will keep
   # the first row with the duplicated value thus keeping the row with the
   # highest absolute value of the t-statistic
   dplyr::distinct(gene_symbols, .keep_all = TRUE)
 
 # Let's create a named vector ranked based on the t-statistic values
-f_vector <- filtered_dge_mapped_df$F
-names(f_vector) <- filtered_dge_mapped_df$gene_symbols
+t_vector <- filtered_dge_mapped_df[,11]
+names(t_vector) <- filtered_dge_mapped_df$gene_symbols
 
 # We need to sort the t-statistic values in descending order here
-f_vector <- sort(f_vector, decreasing = TRUE)
+t_vector <- sort(t_vector, decreasing = TRUE)
 
 set.seed(2022)
 gsea_results <- GSEA(
-  geneList = f_vector, # Ordered ranked gene list
+  geneList = t_vector, # Ordered ranked gene list
   minGSSize = 25, # Minimum gene set size
-  maxGSSize = 500, # Maximum gene set set
-  pvalueCutoff = 0.05, # p-value cutoff
+  maxGSSize = 500, # Maximum gene set set,
+ pvalueCutoff = 0.05,# p-value cutoff
   eps = 0, # Boundary for calculating the p-value
   seed = TRUE, # Set seed to make results reproducible
   pAdjustMethod = "BH", # Benjamini-Hochberg correction
   TERM2GENE = dplyr::select(
     Mm_hallmark_df,
+    gs_name,
     gene_symbol,
-    gs_name
   )
 )
+
+
