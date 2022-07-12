c6a56f07c32e60e10ac4e2917bf3b97f61e9649f
Amenaelmaadawy <en268@stud.uni-heidelberg.de>
2022-07-12 11:24:47 +0200

    code

--- project/gseafoureight.R
+++ project/gseafoureight.R
@@ -1,10 +1,18 @@
-## GSEA trial
-# install packages
-#BiocManager::install("clusterProfiler", update = FALSE)
-#iocManager::install("msigdbr", update = FALSE)
-#BiocManager::install("org.Mm.eg.db", update = FALSE)
+#######DGE und GSEA nur 4-8
+library(limma)
+designfe= model.matrix(~0+factor(c(1,1,1,2,2,2)))
+colnames(designfe)= c("four", "eight")
+fit_matchedfe= lmFit(TRAs_mousedataset[,6:11], designfe)
+contrast.matrixfe= makeContrasts(eight-four,levels = designfe)
+fit_matchedfe2= contrasts.fit(fit_matchedfe, contrast.matrixfe)
+fit_matchedfe2= eBayes(fit_matchedfe2)
+topTable(fit_matchedfe2, coef=1)
+#linear model is done
+dge_dffe=topTable(fit_matchedfe2, coef=1, number=nrow(fit_matchedfe2))
+resultsfe= decideTests(fit_matchedfe2)
+View(resultsfe)
 
-## library
+#####GSEA
 library(clusterProfiler)
 
 # Package that contains MSigDB gene sets in tidy format
@@ -16,35 +24,13 @@ library(org.Mm.eg.db)
 # We will need this so we can use the pipe: %>%
 library(magrittr)
 
-load("fit_matched2.RData")
-####after ian's comment
-dge.heart= DGE_tissues[DGE_tissues$max.tissue=="heart",]
-Transcriptalle = rownames(dge_df)
-dge_df_alle= cbind(Transcriptalle,dge_df)
-
-
-
-
-#projectPath <- dirname(rstudioapi::getSourceEditorContext()$path)
-file.exists("fit.tsv")
-#dge_df <- readr::read_tsv("fit.tsv")
-#dge_df=topTable(fit_matched2, coef=1, number=nrow(fit_matched2))
-Mm_hallmark_df <- msigdbr(
-  species = "Mus musculus", # Replace with species name relevant to your data
-  category = "H")
-head(Mm_hallmark_df)
-keytypes(org.Mm.eg.db)
-dge_heart= subset(dge_df, rownames(dge_df) %in% rownames(dge.heart))
-Transcript = rownames(dge_heart)
-dge_heart= cbind(Transcript,dge_heart)
-
-# First let's create a mapped data frame we can join to the differential
-# expression stats
-dge_mapped_df <- data.frame(
+transcriptsfe = rownames(dge_dffe)
+dge_dffe= cbind(transcriptsfe,dge_dffe)
+dge_femapped_df <- data.frame(
   gene_symbols = mapIds(
     # Replace with annotation package for the organism relevant to your data
     org.Mm.eg.db,
-    keys =rownames(dge_df_alle),
+    keys =rownames(dge_dffe),
     # Replace with the type of gene identifiers in your data
     keytype = "ENSEMBLTRANS",
     # Replace with the type of gene identifiers you would like to map to
@@ -59,39 +45,39 @@ dge_mapped_df <- data.frame(
   # Make an `Ensembl` column to store the rownames
   tibble::rownames_to_column("Ensembltrans") %>%
   # Now let's join the rest of the expression data
-  dplyr::inner_join(dge_df_alle, by = c("Ensembltrans" = "Transcriptalle"))
-head(dge_mapped_df)
+  dplyr::inner_join(dge_dffe, by = c("Ensembltrans" = "transcriptsfe"))
+head(dge_femapped_df)
 #The goal of GSEA is to detect situations where many genes in a gene set change in a coordinated way, even when individual changes are small in magnitude (Subramanian et al. 2005).
 
-any(duplicated(dge_mapped_df$gene_symbols))
-dup_gene_symbol <- dge_mapped_df %>%
+any(duplicated(dge_femapped_df$gene_symbols))
+dup_gene_symbol <- dge_femapped_df %>%
   dplyr::filter(duplicated(gene_symbols)) %>%
   dplyr::pull(gene_symbols)
 
 View(dup_gene_symbol)
 #t test
 
-filtered_dge_mapped_df <- dge_mapped_df %>%
+filtered_dge_femapped_df <- dge_femapped_df %>%
   # Sort so that the highest absolute values of the t-statistic are at the top
-  dplyr::arrange(dplyr::desc(abs(dge_mapped_df[,4]))) %>%
+  dplyr::arrange(dplyr::desc(abs(dge_femapped_df[,4]))) %>%
   # Filter out the duplicated rows using `dplyr::distinct()`-- this will keep
   # the first row with the duplicated value thus keeping the row with the
   # highest absolute value of the t-statistic
   dplyr::distinct(gene_symbols, .keep_all = TRUE)
 
 # Let's create a named vector ranked based on the t-statistic values
-t_vector <- filtered_dge_mapped_df[,4]
-names(t_vector) <- filtered_dge_mapped_df$gene_symbols
+Fcfe_vector <- filtered_dge_femapped_df[,4]
+names(Fcfe_vector) <- filtered_dge_femapped_df$gene_symbols
 
 # We need to sort the t-statistic values in descending order here
-t_vector <- sort(t_vector, decreasing = TRUE)
+Fcfe_vector <- sort(Fcfe_vector, decreasing = TRUE)
 
 set.seed(2022)
-gsea_results <- GSEA(
-  geneList = t_vector, # Ordered ranked gene list
+gsea_feresults <- GSEA(
+  geneList = Fcfe_vector, # Ordered ranked gene list
   minGSSize = 25, # Minimum gene set size
   maxGSSize = 500, # Maximum gene set set,
- pvalueCutoff = 0.05,# p-value cutoff
+  pvalueCutoff = 0.05,# p-value cutoff
   eps = 0, # Boundary for calculating the p-value
   seed = TRUE, # Set seed to make results reproducible
   pAdjustMethod = "BH", # Benjamini-Hochberg correction
@@ -102,5 +88,4 @@ gsea_results <- GSEA(
   )
 )
 
-head(gsea_results@result)
-saveRDS(gsea_results, file="Gsea_mouse.rds")
+head(gsea_feresults@result)
