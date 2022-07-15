7b885c450d012e29244fa80868b2804cb98c3b46
Amenaelmaadawy <en268@stud.uni-heidelberg.de>
2022-07-15 18:27:07 +0200

    heart_genes und so

---
+++ GENES.R
@@ -0,0 +1,11 @@
+
+
+TRA_new = TRA_gene_matrix[!duplicated(TRA_gene_matrix$gene.symbol),]
+View(TRA_new)
+df = data.frame(TRA_new$ensembl.transcript, TRA_new$gene.symbol, TRA_new$max.tissue)
+View(df)
+heart_genes= df[df$TRA_new.max.tissue=="heart",]
+View(heart_genes)
+write.csv(heart_genes,"heart_genes.csv", row.names= FALSE)
+
+
